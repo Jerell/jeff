b630f468bb63776ecea72ed7edd89bd3b7d3fa70
Jerell James <jerell1998@gmail.com>
2022-09-08 20:42:48 +0100

    companies

--- lib/firebase/reviews/IReview.ts
+++ lib/firebase/reviews/IReview.ts
@@ -1,6 +1,7 @@
 export default interface IReview {
   rating: number;
   review: string;
+  company: string;
   name: string;
   date: Date;
   approved: boolean;
@@ -9,6 +10,7 @@ export default interface IReview {
 export interface IReviewData {
   rating: number;
   review: string;
+  company: string;
   name: string;
   date: { nanoseconds: number; seconds: number };
   approved: boolean;
